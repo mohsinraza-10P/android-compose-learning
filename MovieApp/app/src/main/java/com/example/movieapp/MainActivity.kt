7a1ba6c99431e0a42f19ef8c78715f5162f21898
Mohsin Raza <mohsin.raza@10pearls.com>
2024-09-19 19:26:21 +0500

    MovieApp - Scaffold

--- MovieApp/app/src/main/java/com/example/movieapp/MainActivity.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.movieapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,16 +38,41 @@ private fun Preview() {
     App()
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun App() {
     MovieAppTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+         Scaffold(
+             modifier = Modifier.fillMaxSize(),
+             topBar = {
+                 TopAppBar(
+                     title = { Text(text = "Movie App") },
+                     navigationIcon = {
+                         IconButton(
+                             onClick = {
+                                 Log.d("MovieTAG", "Navigation Icon Clicked")
+                             }
+                         ) {
+                             Icon(Icons.Default.Close, contentDescription = null)
+                         }
+                     },
+                     colors = TopAppBarDefaults.topAppBarColors().copy(
+                         containerColor = MaterialTheme.colorScheme.primaryContainer,
+                         titleContentColor = MaterialTheme.colorScheme.onBackground,
+                     )
+                 )
+             }
+         ) { innerPadding ->
             Body(modifier = Modifier.padding(innerPadding))
-        }
+         }
     }
 }
 
 @Composable
 private fun Body(modifier: Modifier = Modifier) {
-    Text(text = "Movie App!")
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+    }
 }
