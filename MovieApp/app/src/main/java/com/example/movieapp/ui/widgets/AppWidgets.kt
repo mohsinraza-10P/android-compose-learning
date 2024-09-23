9d92f1e875747ab94b2e6dae0ef3be5b7f6b52c6
Mohsin Raza <mohsin.raza@10pearls.com>
2024-09-23 20:02:03 +0500

    MovieApp - Movie item made expandable

--- MovieApp/app/src/main/java/com/example/movieapp/ui/widgets/AppWidgets.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/ui/widgets/AppWidgets.kt
@@ -1,100 +1,166 @@
 package com.example.movieapp.ui.widgets
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import coil.transform.CircleCropTransformation
 import coil.transform.RoundedCornersTransformation
 import com.example.movieapp.data.models.Movie
 
 @Composable
 fun MovieRow(movie: Movie, onMovieClick: (String) -> Unit = {}) {
     val context = LocalContext.current
+    var expanded by remember {
+        mutableStateOf(false)
+    }
 
-    Card(modifier = Modifier
-        .padding(vertical = 16.dp)
-        .fillMaxWidth(),
+    Card(
+        modifier = Modifier
+            .padding(vertical = 16.dp)
+            .fillMaxWidth(),
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
         colors = CardDefaults.cardColors().copy(containerColor = Color.White),
         onClick = {
             onMovieClick.invoke(movie.id)
         }) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
+        Row(modifier = Modifier.padding(end = 8.dp)) {
             Surface(
                 modifier = Modifier
                     .size(100.dp)
-                    .padding(8.dp),
+                    .padding(8.dp)
+                    .align(alignment = Alignment.Top),
                 shape = RectangleShape
             ) {
-                /*
-                Icon(
-                    modifier = Modifier.padding(2.dp),
-                    imageVector = Icons.Default.AccountCircle,
-                    contentDescription = "Movie Image",
-                    tint = MaterialTheme.colorScheme.primary
-                )
-                 */
                 AsyncImage(
                     model = ImageRequest.Builder(context)
                         .data(movie.images.first())
                         .crossfade(true)
-                        .transformations(RoundedCornersTransformation(
-                            topLeft = 8f,
-                            topRight = 8f,
-                            bottomLeft = 8f,
-                            bottomRight = 8f,
-                        ))
-                        .build(),
+                        .transformations(
+                            RoundedCornersTransformation(
+                                topLeft = 8f,
+                                topRight = 8f,
+                                bottomLeft = 8f,
+                                bottomRight = 8f,
+                            )
+                        ).build(),
                     contentDescription = movie.title,
                     contentScale = ContentScale.Crop,
                 )
             }
-            Column(modifier = Modifier.padding(all = 8.dp)) {
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .weight(1f) // Use weight to make the column take remaining space, pushing the expand icon to the end.
+            ) {
                 Text(
                     text = movie.title,
                     color = MaterialTheme.colorScheme.onBackground,
                     style = MaterialTheme.typography.titleLarge,
+                    maxLines = 1, // Limit the title to one line
+                    overflow = TextOverflow.Ellipsis // Use ellipsis if the text is too long
                 )
                 Text(
-                    text = "Director: ${movie.director}",
-                    color = Color.Gray,
+                    text = "Genre: ${movie.genre}",
                     style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
                 Text(
                     text = "Released: ${movie.year}",
-                    color = Color.Gray,
                     style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        Text(
+                            text = buildAnnotatedString {
+                                withStyle(
+                                    style = SpanStyle(
+                                        color = Color.DarkGray,
+                                        fontSize = 12.sp,
+                                        fontWeight = FontWeight.Bold,
+                                    )
+                                ) {
+                                    append("Plot: ")
+                                }
+                                withStyle(
+                                    style = SpanStyle(
+                                        color = Color.DarkGray,
+                                        fontSize = 12.sp,
+                                    )
+                                ) {
+                                    append(movie.plot)
+                                }
+                            },
+                            lineHeight = 18.sp,
+                        )
+                        HorizontalDivider(modifier = Modifier.padding(vertical = 4.dp))
+                        Text(
+                            text = "Director: ${movie.director}",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                        Text(
+                            text = "Actors: ${movie.actors}",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                        Text(
+                            text = "Rating: ${movie.rating}",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
+                }
             }
+            Spacer(modifier = Modifier.width(16.dp)) // Add extra space between text and icon
+            Icon(
+                modifier = Modifier
+                    .size(24.dp)
+                    .align(alignment = Alignment.CenterVertically)
+                    .clickable {
+                        expanded = !expanded
+                    },
+                imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = "${if (expanded) "Collapse" else "Expand"} Icon",
+                tint = Color.DarkGray
+            )
         }
     }
 }
