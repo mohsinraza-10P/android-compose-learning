4f7860cb1cf56ea7b8c796e0482db2382b439ff5
Mohsin Raza <mohsin.raza@10pearls.com>
2024-09-24 14:04:10 +0500

    NoteApp - Initial commit

---
+++ NoteApp/app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.noteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.noteapp.ui.theme.NoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // enableEdgeToEdge()
+        setContent {
+            App()
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun Preview() {
+    App()
+}
+
+@Composable
+private fun App() {
+    NoteAppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Body(modifier = Modifier.padding(innerPadding))
+        }
+    }
+}
+
+@Composable
+private fun Body(modifier: Modifier = Modifier) {
+    Text(text = "Note App!")
+}
