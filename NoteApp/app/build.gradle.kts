330bc395891f27ea1a727edd2051d8053500f523
Mohsin Raza <mohsin.raza@10pearls.com>
2024-09-27 10:53:46 +0500

    NoteApp - Lifecycle viewmodel dependency renamed

--- NoteApp/app/build.gradle.kts
+++ NoteApp/app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     implementation(libs.room.android)
